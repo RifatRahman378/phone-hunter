21873f212daa6532d6b21772f991d22d98edec13
Rifat Rahman <rifat.rahman378@gmail.com>
2024-04-30 14:33:53 +0600

    default phones

--- js/phone.js
+++ js/phone.js
@@ -1,4 +1,4 @@
-const dataLoad = async (searchText, isShowAll) => {
+const dataLoad = async (searchText = 'iphone', isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     const phones = data.data;
